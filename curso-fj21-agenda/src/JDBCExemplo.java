7fceab92717697a83ba360f42a868ac73a5f94d5
roniesv <ronie.sousavieira@gmail.com>
2020-09-09 17:28:57 -0300

    conexão ao banco teste.

---
+++ curso-fj21-agenda/src/JDBCExemplo.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class JDBCExemplo {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/test?useTimezonetrue&serverTimezone=UTC","ronie","admin3393");
+		System.out.println("Conectado!");
+		conexao.close();	
+	}
+}
