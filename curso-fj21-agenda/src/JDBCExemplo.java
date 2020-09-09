89f09a65b2ad5ffd5c4845267f1b6d78adf6e931
roniesv <ronie.sousavieira@gmail.com>
2020-09-09 18:00:56 -0300

    add comentário sobre DriverManager

--- curso-fj21-agenda/src/JDBCExemplo.java
+++ curso-fj21-agenda/src/JDBCExemplo.java
@@ -9,7 +9,7 @@ public class JDBCExemplo {
 		//Class é usada para fazer uma representação do objeto necessário até o java 5, após não é necessário, mas é necessário também em servlet container as vezes.
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		//DriverManager é a classe concreta que implementa a interface JDBC
+		//DriverManager é a classe concreta que implementa a interface JDBC basta passar a string de conexão
 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/test?useTimezonetrue&serverTimezone=UTC","ronie","admin3393");
 		System.out.println("Conectado!");
 		conexao.close();	
