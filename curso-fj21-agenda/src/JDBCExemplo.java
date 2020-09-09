78eff7846b4e4445640b72a34ead1ad6f6370c6c
roniesv <55118680+roniesv@users.noreply.github.com>
2020-09-09 17:38:25 -0300

    Update JDBCExemplo.java
    comentários sobre Class

--- curso-fj21-agenda/src/JDBCExemplo.java
+++ curso-fj21-agenda/src/JDBCExemplo.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 public class JDBCExemplo {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		//Class é usada para fazer uma representação do objeto necessário até o java 5, após não é necessário, mas é necessário também em servlet container as vezes.
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/test?useTimezonetrue&serverTimezone=UTC","ronie","admin3393");
