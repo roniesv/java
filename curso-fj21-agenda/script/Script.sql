bf707b4a4d8ffb05f221b74b1d139d4292db7b2f
roniesv <ronie.sousavieira@gmail.com>
2020-09-17 14:38:29 -0300

    conectando ao banco exemplo básico 4.

--- curso-fj21-agenda/script/Script.sql
+++ curso-fj21-agenda/script/Script.sql
@@ -1,2 +1,24 @@
 --<ScriptOptions statementTerminator=";"/>
 
+--exemplo no mysql.
+create table contatos(
+	id bigint not null auto_incremente;
+	nome varchar (255),
+	email varchar(255),
+	dataNascimento date,
+	primary key(id)
+
+);
+
+create table contatos(
+    id serial,
+    nome varchar (255),
+	email varchar(255),
+	dataNascimento date,
+	primary key(id) 	
+
+);
+--serial gera sozinha a chave
+insert into contatos(nome, email, datanascimento)
+values
+('test','test@gmail','yyyy-mm-dd')
---
+++ curso-fj21-agenda/src/JDBCExemplo4ConexaoPostgree.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExemplo4ConexaoPostgree {
+	
+	public static void main(String[] args) {
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fj21", "postgres", "admin");
+			System.out.println("Conectado com sucesso, Seja bem vindo ao Postgre");
+			Statement stmt = conn.createStatement();
+			stmt.executeQuery("select * from contatos");
+			ResultSet rs = stmt.getResultSet();
+			while(rs.next()) {
+				System.out.println(rs.getString("nome"));
+				System.out.println(rs.getString("email"));
+				System.out.println(rs.getString("datanascimento"));
+				
+			}
+	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
