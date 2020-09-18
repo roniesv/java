f4e50abf3f662fee25087ebae4dbde4eb1123315
roniesv <ronie.sousavieira@gmail.com>
2020-09-17 21:55:49 -0300

    Java beans e contato dao ex.2.11

--- curso-fj21-agenda/src/com/caleum/jdbc/ConnectionFactory.java
+++ curso-fj21-agenda/src/com/caleum/jdbc/ConnectionFactory.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
-	public Connection getConection() {
+	public Connection getConnection() {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost/fj21", "root", "admin");
+			return DriverManager.getConnection("jdbc:mysql://localhost/fj21?useTimezonetrue&serverTimezone=UTC", "root", "admin");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			throw new RuntimeException(e);
 		}
 	}
