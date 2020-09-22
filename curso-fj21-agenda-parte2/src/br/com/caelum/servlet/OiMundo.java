3e01b4c441084e02b730126fdd85faa1ce0c5495
roniesv <ronie.sousavieira@gmail.com>
2020-09-22 11:15:17 -0300

    servlet 3.0

--- curso-fj21-agenda-parte2/src/br/com/caelum/servlet/OiMundo.java
+++ curso-fj21-agenda-parte2/src/br/com/caelum/servlet/OiMundo.java
@@ -1,4 +1,4 @@
-package com.caelum.servlet;
+package br.com.caelum.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,18 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class oiMundo extends HttpServlet{
-	
+public class OiMundo extends HttpServlet{
+	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		
 		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira Servlet</title");
+		out.println("</head>");
 		out.println("<body>");
-		out.println("<Primeira servlet>");
+		out.println("<h1>Oi mundo Servlet</h1>");
 		out.println("</body>");
 		out.println("</html>");
-		
-		
+				
 	}
 
 }
