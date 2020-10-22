b5825bf3fce06080339f0016a948fb3751ab361e
roniesv <ronie.sousavieira@gmail.com>
2020-10-22 18:22:48 -0300

    adiciona pela lista de contatos

--- curso-fj21-agenda-parte2/src/com/caelum/mvc/logica/PreAdicionaLogic.java
+++ curso-fj21-agenda-parte2/src/com/caelum/mvc/logica/PreAdicionaLogic.java
@@ -4,10 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class PreAdicionaLogic implements Logica{
-
-	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		return "/WEB-INF/jsp/adiciona-contato2.jsp";
+		return "/WEB-INF/jsp/Adiciona-contato2.jsp";
 	}
 	
 
