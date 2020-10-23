ea3468614ce80e14097480e79dc42c0a523643a3
roniesv <ronie.sousavieira@gmail.com>
2020-10-22 22:31:29 -0300

    formulario para adicionar pela lista

--- curso-fj21-agenda-parte2/src/com/caelum/mvc/logica/PreAdiciona.java
+++ curso-fj21-agenda-parte2/src/com/caelum/mvc/logica/PreAdiciona.java
@@ -3,10 +3,12 @@ package com.caelum.mvc.logica;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PreAdicionaLogic implements Logica{
+public class PreAdiciona implements Logica{
+
+	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO Auto-generated method stub
 		return "/WEB-INF/jsp/adiciona-contato2.jsp";
 	}
-	
 
 }
