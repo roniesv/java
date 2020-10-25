f7a2d1df8910befc6bac109b988904880e0c6f3b
roniesv <ronie.sousavieira@gmail.com>
2020-10-24 23:14:26 -0300

    começamos a usar os filtros

---
+++ curso-fj21-agenda-parte2/src/com/caelum/mvc/logica/AlteraAdicionaLogic.java
@@ -0,0 +1,13 @@
+package com.caelum.mvc.logica;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AlteraAdicionaLogic implements Logica{
+
+	
+	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		return "";
+	}
+
+}
