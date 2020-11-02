129aabf09161d831a88bd564e9c15bf12b8aae36
roniesv <ronie.sousavieira@gmail.com>
2020-11-01 21:19:03 -0300

    conexao agora como filtro

---
+++ curso-fj21-agenda-parte2/src/com/caelum/agenda/filtro/FiltroConexao.java
@@ -0,0 +1,54 @@
+package com.caelum.agenda.filtro;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import com.caelum.agenda.jdbc.ConnectionFactory;
+
+
+@WebFilter("/*")
+public class FiltroConexao implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request , ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try {
+			Connection connection = new ConnectionFactory().getConnection();
+			request.setAttribute("conexao", connection);
+			chain.doFilter(request, response);
+			
+			connection.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new ServletException(e);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
