b0e590b6e94d3ecbc7143aa0267392a1a0110547
roniesv <ronie.sousavieira@gmail.com>
2021-01-19 10:31:24 -0300

    implementação busca simples em cliente

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,13 +1,14 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -24,6 +25,16 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 			return clienteRepository.findAll();
+		//return clienteRepository.findByNome("konoramaru");
+        //return clienteRepository.findByNomeContaining("me");  
+		
 	}
-
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		
+		return cliente.orElse(null);
+	}
+	
 }
